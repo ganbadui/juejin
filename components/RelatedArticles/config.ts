ca2518b41fbb810b2d628aa9d91615ee02295a5e
forcejie <2963301696@qq.com>
2023-02-09 18:01:46 +0800

    feat: 相关文章组件完整

---
+++ components/RelatedArticles/config.ts
@@ -0,0 +1,27 @@
+export const Articles = [
+  {
+    article: '今日头条 ANR 优化实践系列 - Barrier 导致主线程假死',
+    lover: 200,
+    comment: 10
+  },
+  {
+    article: '组织架构数据同步效率优化，百倍提速',
+    lover: 87,
+    comment: 4
+  },
+  {
+    article: '手把手教会你小程序登录鉴权',
+    lover: 233,
+    comment: 22
+  },
+  {
+    article: '记一次JAVA使用Ldap操作AD域',
+    lover: 9,
+    comment: 0
+  },
+  {
+    article: '你真的了解tree shaking吗',
+    lover: 56,
+    comment: 1
+  }
+]
