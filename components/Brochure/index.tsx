4948f2b86db2e1d381c9a224e28a2ba377019ef7
forcejie <2963301696@qq.com>
2023-02-14 22:02:29 +0800

    feat: 使用Affix固钉将广告固定

--- components/Brochure/index.tsx
+++ components/Brochure/index.tsx
@@ -6,17 +6,24 @@ import AuthorList from './cpns/AuthorList'
 import GuideBook from './cpns/GuideBook'
 import styles from './index.module.scss'
 import Arts from './cpns/Arts'
+import { Affix } from 'antd'
+import { useState } from 'react'
 
 export interface IProps {
   children?: ReactElement
 }
 const Brochure: FC<IProps> = memo(props => {
+  const [top, setTop] = useState(120)
   const { children } = props
   return (
     <div className={styles.brochure}>
       <Footer></Footer>
-      <Arts></Arts>
-      <Advertisement></Advertisement>
+
+      <Affix offsetTop={top}>
+        <Arts></Arts>
+        <Advertisement></Advertisement>
+      </Affix>
+
       <AuthorList></AuthorList>
       <GuideBook></GuideBook>
     </div>
