df8f5133467c468452523ffad10ab76ae8c2ef87
cjhw <1784581164@qq.com>
2023-02-22 19:56:48 +0800

    feat: 删除多余的逻辑

--- components/articleList/index.tsx
+++ components/articleList/index.tsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import styles from './index.module.scss'
 
 import { EyeOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons'
-import { Divider, List, Skeleton } from 'antd'
+import { List } from 'antd'
 import Link from 'next/link'
 import { IconText } from '@/components'
 import { NextPage } from 'next'
 import formatTime from '@/utils/formatTime'
 import { useRouter } from 'next/router'
-import InfiniteScroll from 'react-infinite-scroll-component'
-import { getAList } from '@/service/articleData'
 
 export interface ListItem {
   id: number
@@ -35,40 +33,9 @@ const ArticleList: NextPage<IProps> = ({ listData }) => {
   }
 
   const router = useRouter()
-  const [loading, setLoading] = useState(false)
-
-  const data = [...listData]
-  //使得data动态更新
-
-  const [list, setList] = useState(data)
-  const [page, setPage] = useState(2)
-  const loadMoreData = async () => {
-    if (loading) {
-      return
-    }
-    setLoading(true)
-
-    const pageSize = 10
-    // 调用文章列表接口
-    const newData: any = await (await getAList(page, pageSize)).data
-
-    // 更新数据
-    setList(list.concat(newData))
-
-    setLoading(true)
-    console.log(list)
-
-    // 更新页码
-    setPage(page + 1)
-
-    console.log(page)
-  }
-  useEffect(() => {
-    loadMoreData()
-  }, [])
 
   return (
-    <div id="scrollableDiv" className={styles.articleList}>
+    <div className={styles.articleList}>
       <header className={`${styles.list_header}`}>
         {listTopData.map(label => (
           <span
@@ -82,70 +49,59 @@ const ArticleList: NextPage<IProps> = ({ listData }) => {
       </header>
 
       <div className={styles.list_content}>
-        <InfiniteScroll
-          dataLength={list.length}
-          next={loadMoreData}
-          hasMore={list.length < 15}
-          loader={<Skeleton paragraph={{ rows: 1 }} active />}
-          endMessage={<Divider plain>没有更多了 🤐</Divider>}
-          scrollableTarget="scrollableDiv"
-        >
-          <List
-            itemLayout="vertical"
-            size="large"
-            dataSource={list}
-            renderItem={(item: ListItem) => (
-              <List.Item
-                key={item.id}
-                className={styles.list_item}
-                onClick={() => router.push(`/article/${item.id}`)}
-              >
-                <div className={styles.list_item_header}>
-                  <Link href={'/'} className={styles.list_item_header_author}>
-                    {item.author}
-                  </Link>
-                  <div className={styles.list_item_header_date}>
-                    {formatTime(item.publishTime)}
-                  </div>
-                  <span className={styles.list_item_header_tag}>
-                    {item.tag}
-                  </span>
+        <List
+          itemLayout="vertical"
+          size="large"
+          dataSource={listData}
+          renderItem={(item: ListItem) => (
+            <List.Item
+              key={item.id}
+              className={styles.list_item}
+              onClick={() => router.push(`/article/${item.id}`)}
+            >
+              <div className={styles.list_item_header}>
+                <Link href={'/'} className={styles.list_item_header_author}>
+                  {item.author}
+                </Link>
+                <div className={styles.list_item_header_date}>
+                  {formatTime(item.publishTime)}
                 </div>
-                <div className={styles.list_item_content}>
-                  <div className={styles.list_item_content_left}>
-                    <List.Item.Meta
-                      title={<Link href={''}>{item.title}</Link>}
-                      description={item.description}
+                <span className={styles.list_item_header_tag}>{item.tag}</span>
+              </div>
+              <div className={styles.list_item_content}>
+                <div className={styles.list_item_content_left}>
+                  <List.Item.Meta
+                    title={<Link href={''}>{item.title}</Link>}
+                    description={item.description}
+                  />
+                  <div className={styles.list_item_content_left_icon}>
+                    <IconText
+                      icon={EyeOutlined}
+                      text="156"
+                      key="list-vertical-star-o"
                     />
-                    <div className={styles.list_item_content_left_icon}>
-                      <IconText
-                        icon={EyeOutlined}
-                        text="156"
-                        key="list-vertical-star-o"
-                      />
 
-                      <IconText
-                        icon={LikeOutlined}
-                        text="156"
-                        key="list-vertical-like-o"
-                      />
+                    <IconText
+                      icon={LikeOutlined}
+                      text="156"
+                      key="list-vertical-like-o"
+                    />
 
-                      <IconText
-                        icon={MessageOutlined}
-                        text="2"
-                        key="list-vertical-message"
-                      />
-                    </div>
+                    <IconText
+                      icon={MessageOutlined}
+                      text="2"
+                      key="list-vertical-message"
+                    />
                   </div>
-
-                  {item.avatar && (
-                    <img className={styles.list_item_thumb} src={item.avatar} />
-                  )}
                 </div>
-              </List.Item>
-            )}
-          />
-        </InfiniteScroll>
+
+                {item.avatar && (
+                  <img className={styles.list_item_thumb} src={item.avatar} />
+                )}
+              </div>
+            </List.Item>
+          )}
+        />
       </div>
     </div>
   )
